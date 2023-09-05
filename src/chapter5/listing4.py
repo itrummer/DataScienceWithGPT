f1dac9d8096bfba5f0b8f7d24c466191fbd8eb29
itrummer <immanuel.trummer@gmail.com>
2023-09-05 11:33:20 -0400

    Added listings containing output.

---
+++ src/chapter5/listing4.py
@@ -0,0 +1,24 @@
+'''
+Created on Sep 5, 2023
+
+@author: immanueltrummer
+'''
+{
+  "choices": [
+    {
+      "finish_reason": "length",
+      "index": 0,
+      "logprobs": null,
+      "text":"\n\nOnce upon a time, there was a kingdom ruled by a wise and"
+    }
+  ],
+  "created": 1679517955,
+  "id": "cmpl-...",
+  "model": "text-davinci-003",
+  "object": "text_completion",
+  "usage": {
+    "completion_tokens": 16,
+    "prompt_tokens": 5,
+    "total_tokens": 21
+  }
+}
