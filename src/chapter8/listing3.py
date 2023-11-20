79aa95b86f23a88dad09e721df9a5b1eadc85cd1
itrummer <immanuel.trummer@gmail.com>
2023-11-20 18:02:07 -0500

    Re-organizing code between chapter 8 and chapter 9.

--- src/chapter8/listing3.py
+++ src/chapter8/listing3.py
@@ -1,35 +1,89 @@
 '''
-Created on Nov 10, 2023
+Created on Nov 11, 2023
 
 @author: immanueltrummer
 '''
 import argparse
+import cv2
+import base64
 import openai
+import time
 
 
-def transcribe(audio_path):
-    """ Transcribe audio file to text.
+def extract_frames(video_path):
+    """ Extracts frames from a video.
     
     Args:
-        audio_path: path to audio file.
+        video_path: path to video file.
     
     Returns:
-        transcribed text.
+        list of first ten video frames.
     """
-    with open(audio_path, 'rb') as audio_file:
-        transcription = openai.Audio.transcribe(
-            file=audio_file, model='whisper-1')
-        return transcription.text
+    video = cv2.VideoCapture(video_path)
+    frames = []
+    while video.isOpened() and len(frames) <= 10:
+        success, frame = video.read()
+        if not success:
+            break
+        
+        _, buffer = cv2.imencode('.jpg', frame)
+        encoded = base64.b64encode(buffer)
+        frame = encoded.decode('utf-8')
+        frames += [frame]
+    
+    video.release()
+    return frames
+
+
+def create_prompt(frames):
+    """ Create prompt to generate title for video.
+    
+    Args:
+        frames: frames of video.
+    
+    Returns:
+        prompt containing multimodal data (as list).
+    """
+    prompt = ['Generate a concise title for the video.']
+    for frame in frames[:10]:
+        element = {'image':frame, 'resize':768}
+        prompt += [element]
+    return prompt
+
+
+def call_llm(prompt):
+    """ Query large language model and return answer.
+    
+    Args:
+        prompt: input prompt for language model.
+    
+    Returns:
+        Answer by language model.
+    """
+    for nr_retries in range(1, 4):
+        try:
+            response = openai.ChatCompletion.create(
+                model='gpt-4-vision-preview',
+                messages=[
+                    {'role':'user', 'content':prompt}
+                    ]
+                )
+            return response['choices'][0]['message']['content']
+        except:
+            time.sleep(nr_retries * 2)
+    raise Exception('Cannot query OpenAI model!')
 
 
 if __name__ == '__main__':
-    
+
     parser = argparse.ArgumentParser()
     parser.add_argument('openaikey', type=str, help='OpenAI access key')
-    parser.add_argument('audiopath', type=str, help='Path to audio file')
+    parser.add_argument('videopath', type=str, help='Path of video file')
     args = parser.parse_args()
-
+    
     openai.api_key = args.openaikey
-
-    transcript = transcribe(args.audiopath)
-    print(transcript)
+    
+    frames = extract_frames(args.videopath)
+    prompt = create_prompt(frames)
+    title = call_llm(prompt)
+    print(title)
---
+++ src/chapter9/listing3.py
@@ -0,0 +1,35 @@
+'''
+Created on Nov 10, 2023
+
+@author: immanueltrummer
+'''
+import argparse
+import openai
+
+
+def transcribe(audio_path):
+    """ Transcribe audio file to text.
+    
+    Args:
+        audio_path: path to audio file.
+    
+    Returns:
+        transcribed text.
+    """
+    with open(audio_path, 'rb') as audio_file:
+        transcription = openai.Audio.transcribe(
+            file=audio_file, model='whisper-1')
+        return transcription.text
+
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('openaikey', type=str, help='OpenAI access key')
+    parser.add_argument('audiopath', type=str, help='Path to audio file')
+    args = parser.parse_args()
+
+    openai.api_key = args.openaikey
+
+    transcript = transcribe(args.audiopath)
+    print(transcript)
