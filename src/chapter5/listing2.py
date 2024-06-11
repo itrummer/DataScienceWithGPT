8cca12031096abbd116bd04f85b3042d8881ec97
itrummer <immanuel.trummer@gmail.com>
2024-06-11 11:39:17 -0400

    Updated code for Chapter 5.

--- src/chapter5/listing2.py
+++ src/chapter5/listing2.py
@@ -1,57 +1,11 @@
 '''
-Created on Sep 5, 2023
+Created on Sep 4, 2023
 
 @author: immanueltrummer
 '''
-{
-  "data": [
-    {
-      "created": 1607632725,
-      "id": "curie:2020-05-03",
-      "object": "model",
-      "owned_by": "system",
-      "parent": null,
-      "permission": [
-        {
-          "allow_create_engine": false,
-          "allow_fine_tuning": false,
-          "allow_logprobs": true,
-          "allow_sampling": true,
-          "allow_search_indices": false,
-          "allow_view": true,
-          "created": 1607632727,
-          "group": null,
-          "id": "snapperm-...",
-          "is_blocking": false,
-          "object": "model_permission",
-          "organization": "*"
-        }
-      ],
-      "root": "curie:2020-05-03"
-    },
-    {
-      "created": 1641833573,
-      "id": "curie:ft-personal-2022-01-10-16-52-53",
-      "object": "model",
-      "owned_by": "user-...",
-      "parent": "curie:2020-05-03",
-      "permission": [
-        {
-          "allow_create_engine": true,
-          "allow_fine_tuning": true,
-          "allow_logprobs": true,
-          "allow_sampling": true,
-          "allow_search_indices": false,
-          "allow_view": true,
-          "created": 1641833573,
-          "group": null,
-          "id": "snapperm-...",
-          "is_blocking": false,
-          "object": "model_permission",
-          "organization": "org-..."
-        }
-      ],
-      "root": "curie:2020-05-03"
-    },
-    ...
-}
+import openai
+openai.api_key = '...'
+result = openai.Completion.create(
+    model='text-davinci-003', 
+    prompt='Tell me a story!')
+print(result)
--- src/chapter5/listing3.py
+++ src/chapter5/listing3.py
@@ -7,5 +7,10 @@ import openai
 openai.api_key = '...'
 result = openai.Completion.create(
     model='text-davinci-003', 
-    prompt='Tell me a story!')
-print(result)
+    prompt='Tell me a story!' +\
+        '\n\nOnce upon a time, there was a kingdom ruled by a wise and',
+        max_tokens=512, stop='happily ever after', 
+        temperature=1, best_of=2, 
+        presence_penalty=0.5,
+        logit_bias={14844:-100})
+print(result['choices'][0]['text'])
